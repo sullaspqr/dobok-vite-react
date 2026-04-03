3bf735db41caeb7d73dc2cfc1c4d2032e7d6b324
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2026-04-03 12:42:26 +0200

    Change form to submit drum data instead of instruments

--- src/New.jsx
+++ src/New.jsx
@@ -4,12 +4,11 @@ export const New = () => {
     const navigate = useNavigate()
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post("http://localhost:3001/instruments", {
+        axios.post("http://localhost:3001/drums", {
             name: event.target.name.value,
             brand: event.target.brand.value,
             price: Number(event.target.price.value),
-            quantity: Number(event.target.quantity.value),
-            imageURL: event.target.imageURL.value
+            quantity: Number(event.target.quantity.value)
         })
         .then(() =>{
             navigate("/");
@@ -20,10 +19,10 @@ export const New = () => {
     }
     return (
         <div className="p-5 content bg-whitesmoke text-center">
-            <h2>Uj hangszer</h2>
+            <h2>Uj dob</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-group row pb-3">
-                    <label className="col-sm-3 col-form-label">Hangszer neve:</label>
+                    <label className="col-sm-3 col-form-label">Dob neve:</label>
                     <div className="col-sm-9">
                         <input type="text" name="name" className="form-control" />
                     </div>
@@ -44,12 +43,6 @@ export const New = () => {
                     <label className="col-sm-3 col-form-label">Darabszám:</label>
                     <div className="col-sm-9">
                         <input type="text" name="quantity" className="form-control" />
-                    </div>
-                </div>
-   <div className="form-group row pb-3">
-                    <label className="col-sm-3 col-form-label">Kép URL:</label>
-                    <div className="col-sm-9">
-                        <input type="text" name="imageURL" className="form-control" />
                     </div>
                 </div>
                 <button type="submit" className="btn btn-success">Küldés</button>
